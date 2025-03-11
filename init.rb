acab30a03466994b6c2d7bd1af8e54511332e1f5
Adriano Baptistella <adrianojosebaptistella@gmail.com>
2025-03-11 08:46:37 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
-require_dependency 'html_head_hook_listener'
+require_relative 'html_head_hook_listener'
 
 Redmine::Plugin.register :bestest_timer do
   name 'Bestest Punch Clock for Redmine'
   author 'Martin Blom'
   description "This is an awesome punch clock/timer plugin for Redmine. It's great. It's the bestest."
-  version '1.2.0'
-  url 'https://github.com/LeviticusMB/bestest_timer'
+  version '1.3.0'
+  url 'https://github.com/adrianobr/bestest_timer'
   author_url 'https://github.com/LeviticusMB'
 end
